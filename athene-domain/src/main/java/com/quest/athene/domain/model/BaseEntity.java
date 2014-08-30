49a4e6f23a3e0e836598928c0fe1cc00066f80f1
imilo <88413945@qq.com>
2014-08-30 18:54:00 +0800

    refactor model

---
+++ athene-domain/src/main/java/com/quest/athene/domain/model/BaseEntity.java
@@ -0,0 +1,57 @@
+package com.quest.athene.domain.model;
+
+import java.util.Date;
+
+/**
+ * Created by milo on 2014/8/30.
+ */
+public abstract class BaseEntity {
+
+    private String isDeleted;
+
+    private long creatorId;
+    private Date createdTime;
+    private long modifierId;
+    private Date modified_time;
+
+    public String getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(String isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public long getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public long getModifierId() {
+        return modifierId;
+    }
+
+    public void setModifierId(long modifierId) {
+        this.modifierId = modifierId;
+    }
+
+    public Date getModified_time() {
+        return modified_time;
+    }
+
+    public void setModified_time(Date modified_time) {
+        this.modified_time = modified_time;
+    }
+
+}
